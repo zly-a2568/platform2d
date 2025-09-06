8fede1a4943b6c43fdfdb353caa772e7a6cfb61b
zly-a <zly-a@outlook.com>
2025-09-06 21:00:03 +0800

    Signed-off-by: zly-a <zly-a@outlook.com>

--- scenes/setting_panel.gd
+++ scenes/setting_panel.gd
@@ -76,10 +76,12 @@ func _on_button_2_pressed() -> void:
 
 
 func _on_check_update_pressed() -> void:
-	if OS.get_name()!="Windows":
-		OS.alert("此功能目前仅对Windows开放")
+	if OS.get_name()!="Android" and OS.get_name()!="Windows":
+		OS.alert("暂时仅对Windows和Android开放")
 		return
 	$VBoxContainer/ScrollContainer/GridContainer/CheckUpdate.disabled=true
+	$VBoxContainer/ScrollContainer/GridContainer/CheckUpdate.text="检查中"
+	$VBoxContainer/Exit.disabled=true
 	var update_url:String
 	if update_source=="github":
 		update_url="https://github.com/zly-a1/platform/releases/download/latest/version-note.txt"
@@ -92,27 +94,49 @@ func _on_check_update_pressed() -> void:
 
 
 func _on_http_request_request_completed(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray) -> void:
-	
+	if result!=HTTPRequest.RESULT_SUCCESS:
+		print("网络请求错误：",result)
+		fail_update()
+		return
 	var remote_version=body.get_string_from_utf8().substr(1)
 	ver=remote_version
 	var current_version=ProjectSettings.get_setting("application/config/version") as String
 	var cur_ver_array=current_version.split(".")
 	var rem_ver_array=remote_version.split(".")
-	for a in range(3):
-		if int(rem_ver_array[a])>int(cur_ver_array[a]):
-			print("update avivable")
-			$ExecutableDownload.download_file=OS.get_executable_path().get_base_dir()+"/platform2d.pck"
-			if OS.get_name()=="Windows":
-				var update_url:String
-				if update_source=="github":
-					update_url="https://github.com/zly-a1/platform/releases/download/latest/platform2d.pck"
+	if OS.get_name()!="Android":
+		for a in range(3):
+			if int(rem_ver_array[a])>int(cur_ver_array[a]):
+				print("update avivable")
+				if OS.get_name()!="Android":
+					$ExecutableDownload.download_file=OS.get_executable_path().get_base_dir()+"/platform2d.pck"
+					var update_url:String
+					if update_source=="github":
+						update_url="https://github.com/zly-a1/platform/releases/download/latest/platform2d.pck"
+					else:
+						update_url="https://gitee.com/zly-k/platformer2d/releases/download/latest/platform2d.pck"
+					$ExecutableDownload.request(update_url)
+					
+					update_downloading=true
+					return
 				else:
-					update_url="https://gitee.com/zly-k/platformer2d/releases/download/latest/platform2d.pck"
-				$ExecutableDownload.request(update_url)
-				
-				update_downloading=true
-			return
+					var download_dir:=OS.get_system_dir(OS.SYSTEM_DIR_DOWNLOADS)+"/platform"
+					if not DirAccess.dir_exists_absolute(download_dir):
+						if DirAccess.make_dir_recursive_absolute(download_dir)!=OK:
+							fail_update()
+							return
+					var download_package=download_dir+"platform2d.apk"
+					$ExecutableDownload.download_file=download_package
+					var update_url:String
+					if update_source=="github":
+						update_url="https://github.com/zly-a1/platform/releases/download/latest/platform2d.apk"
+					else:
+						update_url="https://gitee.com/zly-k/platformer2d/releases/download/latest/platform2d.apk"
+					$ExecutableDownload.request(update_url)
+					update_downloading=true
+					return
+	OS.alert("已是最新版本","提示")
 	$VBoxContainer/ScrollContainer/GridContainer/CheckUpdate.disabled=false
+	$VBoxContainer/Exit.disabled=false
 	pass
 	
 func _process(delta: float) -> void:
@@ -122,14 +146,28 @@ func _process(delta: float) -> void:
 func _on_executable_download_request_completed(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray) -> void:
 	$VBoxContainer/ScrollContainer/GridContainer/CheckUpdate.text="检查"
 	$VBoxContainer/ScrollContainer/GridContainer/CheckUpdate.disabled=false
+	$VBoxContainer/Exit.disabled=false
 	update_downloading=false
-	if not body.get_string_from_utf8().find("404"):
+	if result==HTTPRequest.RESULT_SUCCESS:
 		ProjectSettings.set_setting("application/config/version",ver)
 		settings.set_value("Run","version",ProjectSettings.get_setting("application/config/version") as String)
 		settings.save(GameProcesser.CONFIG_PATH)
+		if OS.get_name()=="Android":
+			var downloaded_file:=OS.get_system_dir(OS.SYSTEM_DIR_DOWNLOADS)+"/platform/platform2d.apk"
+			OS.shell_open(downloaded_file)
+			OS.alert("更新完成，请重启","提示")
+			get_tree().quit()
+	else:
+		fail_update()
+		return
 	
 	pass # Replace with function body.
 
+func fail_update():
+	OS.alert("更新失败","警告")
+	$VBoxContainer/ScrollContainer/GridContainer/CheckUpdate.text="检查"
+	$VBoxContainer/ScrollContainer/GridContainer/CheckUpdate.disabled=false
+	$VBoxContainer/Exit.disabled=false
 
 func _on_option_button_item_selected(index: int) -> void:
 	if index==0:
