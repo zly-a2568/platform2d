f56d4f45c8c17662f987c054a6916349e1dc3041
zly <zly-a@outlook.com>
2026-02-12 00:13:53 +0800

    update to godot 4.6

--- scenes/snow.gd
+++ scenes/snow.gd
@@ -15,3 +15,9 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is Player:
 		player.status.health=0
 	pass # Replace with function body.
+
+
+func _on_replacer_body_entered(body: Node2D) -> void:
+	if body is Player:
+		player.position=Vector2(8800.0,-1800.0)
+	pass # Replace with function body.
